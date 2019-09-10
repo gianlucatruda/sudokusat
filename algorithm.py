95c98f441bea14d3e7360f37ffd25136b9644736
Gianluca Truda <gianlucatruda@gmail.com>
2019-09-10 16:28:38 +0200

    Added skeleton of DPLL algorithm.

---
+++ algorithm.py
@@ -0,0 +1,24 @@
+"""Implementation of DPLL algorithm"""
+
+from simplifications import tautology, unit_clause, pure_literals
+
+
+def dpll(sigma):
+    """Apply DPLL algorithm to some expression `sigma`
+
+    sigma must be a list of lists of integers representing
+    DIMACS notation for the problem.
+
+    """
+
+    # if expression is empty, then SAT
+
+    # if expression contains empty clause, UNSAT
+
+    # Simplify (tautologies, unit clauses, pure literals)
+
+    # Split with recursive call
+
+
+
+    pass
